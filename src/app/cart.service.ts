267039290d2eb13ced5525021e3ad5e1cb6a36be
faizalkhan <faizalkhan.s@gmail.com>
2019-07-09 15:20:25 +0530

    2 files modified, 1 added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class CartService {
+
+ items =[];
+
+ addtocart(product)
+ {
+   this.items.push(product);
+ }
+getItems()
+{
+  return this.items;
+}
+clearItem()
+{
+  this.items =[];
+  return this.items;
+
+}
+
+  constructor() { }
+
+}
